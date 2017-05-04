2c7a8377aa8fd4bbb370f574146c33af85e30967
andrey.demchuk <andreichdem@gmail.com>
2017-05-04 12:29:06 +0300

    Adding PersonClass.java

---
+++ src/main/java/ua/test/gitoverview/Person.java
@@ -0,0 +1,31 @@
+package ua.test.gitoverview;
+
+public class Person  extends BaseEntity{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Id" + super.getId() + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
